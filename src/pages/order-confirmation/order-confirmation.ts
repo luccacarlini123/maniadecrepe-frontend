d6affecbd052c82e2805d92f20e1b547f3765326
Lucca Carlini <lucca.carlini18@gmail.com>
2020-10-08 15:21:09 -0300

    Salvando Pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { CartService } from '../../services/domain/cart-service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public clienteService: ClienteService,
-    public cartService: CartService) {
+    public cartService: CartService,
+    public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
   }
@@ -49,4 +51,24 @@ export class OrderConfirmationPage {
   total() : number {
     return this.cartService.total();
   } 
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        if(response.headers.get('location')){
+          this.cartService.createOrClearCart();
+          console.log(response.headers.get('location'));
+        }
+      },
+      error => {
+        if(error.staus == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
 }
